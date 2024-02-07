2a09410a9f521d592c0c1e1c6b5071b468f62014
SethMegbetor <sethmegbetor@gmail.com>
2024-02-07 11:06:42 +0000

    done with quotes generator

---
+++ quotes_generator/script.js
@@ -0,0 +1,33 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const newQuoteBtn = document.getElementById("new-quote");
+
+let apiQoutes = [];
+
+function newQuote() {
+  // pick a random quote
+  const quote = apiQoutes[Math.floor(Math.random() * apiQoutes.length)];
+
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    let authorName = quote.author.replace(", type.fit", "")
+    authorText.textContent = authorName
+  }
+  quoteText.textContent = quote.text;
+}
+// get quotes
+async function getQuotes() {
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiUrl);
+    apiQoutes = await response.json();
+    newQuote();
+  } catch (error) {
+    // empty
+  }
+}
+
+newQuoteBtn.addEventListener("click", newQuote);
+getQuotes();
